76822970c4fb5052a6b2ff9239756e46bbb48313
Batsirai Swiswa <bswiswa@precipiodx.com>
2016-09-08 10:40:13 -0400

    First look

--- Venture/Analysis.R
+++ Venture/Analysis.R
@@ -4,7 +4,7 @@
 ###############################################################################
 
 # library(xlsx) optional package to read xlsx format. CSV used in this case
-
+install.packages("gmapsdistance")
 # gmaps distance package for time estimates
 library(gmapsdistance)
 # get data
@@ -42,15 +42,17 @@ origins = gsub("(.*PU: )(.*?)(;.*)","\\2",routes)
 destinations = gsub("(.*DO: )(.*?)(;.*)","\\2",routes)
 
 #TODO check with rob that its ok to parse this as follows
-#strip the comment before the comma by looking for the streat number
+#strip the comment before the comma by looking for the street number
 origins[-grep("[Aa]ir", origins)] = gsub("(^.*?, )([0-9].*)","\\2",origins[-grep("[Aa]ir", origins)])
+# eg "Home, 149 Winchester Ave, New Haven, CT " loses the initial "Home"
 destinations[-grep("[Aa]ir", destinations)] = gsub("(^.*?, )([0-9].*)","\\2",destinations[-grep("[Aa]ir", destinations)])
+# same with destinations
 
-#HTML encode the origins and destinations
+#HTML encode the origins and destinations (more suitable to gmapsdistance)
 origins = sapply(origins, URLencode)
 destinations = sapply(destinations, URLencode)
 
-#select only the airports
+#select only the airports (returns all indexes with an airport origin or destination)
 inds = union(grep("[Aa]ir", origins),grep("[Aa]ir", destinations))
 #durations = gmapsdistance(
 #		origin = origins[inds],
