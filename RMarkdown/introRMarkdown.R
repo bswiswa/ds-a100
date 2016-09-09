b992f6dc6c54186195cc0882d7e3721aebba8a48
Batsirai Swiswa <bswiswa@precipiodx.com>
2016-09-09 16:47:54 -0400

    Intro to R Markdown & presentations

---
+++ RMarkdown/introRMarkdown.R
@@ -0,0 +1,13 @@
+# presentations are more dynamic
+# can create presentation as you write your code
+# can generate html files which are dynamic. pdf output is also possible
+# html presentations can always include up to date data for reproducibility
+# copying and pasting is only static
+
+#Markdown started as an easier way to write HTML. Is easy to learn and quick to write
+# Markdown rules
+# #Headings, ## subHeadings
+# *italic*, **bold**, -bullets
+# link to content
+
+# adding slides to built in R Markdown templates
